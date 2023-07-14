cc631cd50ead14d2537fec6edcf2acbada7558ea
gauravchavan1799 <gauravchavan1799@gmail.com>
2023-07-14 19:49:12 +0530

    path added

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-MODEL_FILE_PATH = r'static\models\logistic_classification.pkl'
+MODEL_FILE_PATH = os.path.join(r'static','models','logistic_classification.pkl')
 
-JSON_FILE_PATH = r'static\json_files\proj_data.json'
+JSON_FILE_PATH = os.path.join(r'static','json_files','proj_data.json')
